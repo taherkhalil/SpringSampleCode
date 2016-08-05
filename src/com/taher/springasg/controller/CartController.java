599f5ed887d7543d52e8624dd4f813ddef96e319
taherkhalil <taher.khalil@bitwiseglobal.com>
2016-08-05 20:32:06 +0530

    login page working

--- src/com/taher/springasg/controller/CartController.java
+++ src/com/taher/springasg/controller/CartController.java
@@ -3,6 +3,7 @@ package com.taher.springasg.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,16 +11,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CartController {
 	@RequestMapping(value="/" ,method  = RequestMethod.GET)
-	public String login(){
+	public String login(Model model){
 		
 		return "login";
 	}
-	@RequestMapping(value = "/login" ,method = RequestMethod.GET)
+	@RequestMapping(value = "/usercheck" ,method = RequestMethod.GET)
 	public String validate(ModelMap model , HttpServletRequest request){
 		String username = request.getParameter("username");
 		String pwd = request.getParameter("password");
 		if("taher".equalsIgnoreCase(username) && "1234".equalsIgnoreCase(pwd))
 		{
+			model.addAttribute("message", "Successfully logged in.");
 		 return "firstpage";
 	}
 		return "login";
