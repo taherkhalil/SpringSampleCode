0494693d53b8700772e7d4de88c8975bec322c1a
taherkhalil <taher.khalil@bitwiseglobal.com>
2016-08-05 19:17:28 +0530

    work on login more

--- src/com/taher/springasg/controller/CartController.java
+++ src/com/taher/springasg/controller/CartController.java
@@ -1,5 +1,28 @@
 package com.taher.springasg.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class CartController {
+	@RequestMapping(value="/" ,method  = RequestMethod.GET)
+	public String login(){
+		
+		return "login";
+	}
+	@RequestMapping(value = "/login" ,method = RequestMethod.GET)
+	public String validate(ModelMap model , HttpServletRequest request){
+		String username = request.getParameter("username");
+		String pwd = request.getParameter("password");
+		if("taher".equalsIgnoreCase(username) && "1234".equalsIgnoreCase(pwd))
+		{
+		 return "firstpage";
+	}
+		return "login";
+		}
 
 }
